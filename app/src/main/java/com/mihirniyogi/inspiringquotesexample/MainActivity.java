a3ea6396b51edf2cce7d117858884ae5532816eb
mihirniyogi <E1122052@u.nus.edu>
2025-06-06 00:05:09 +0800

    Add feedback notification

--- app/src/main/java/com/mihirniyogi/inspiringquotesexample/MainActivity.java
+++ app/src/main/java/com/mihirniyogi/inspiringquotesexample/MainActivity.java
@@ -1,19 +1,30 @@
 package com.mihirniyogi.inspiringquotesexample;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.firebase.appdistribution.FirebaseAppDistribution;
+import com.google.firebase.appdistribution.InterruptionLevel;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -25,16 +36,35 @@ public class MainActivity extends AppCompatActivity {
 
     private final OkHttpClient client = new OkHttpClient();
     private final String API_URL = "https://api.api-ninjas.com/v1/quotes";
+    private final String NOTIF_PERM = Manifest.permission.POST_NOTIFICATIONS;
     private TextView quoteTextView;
     private TextView authorTextView;
     private Button fetchQuoteButton;
+    private FirebaseAppDistribution distribution;
+
+    private final ActivityResultLauncher<String> permissionLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            isGranted -> {
+                if (isGranted) {
+                    Log.d("Permission", "Notification granted");
+                    showFeedbackNotification();
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // initialise all text views, buttons
         initialiseUi();
+
+        // request notification permission
+        requestNotifPermission();
+
+        // show notification
+        showFeedbackNotification();
     }
 
     private void initialiseUi() {
@@ -42,6 +72,24 @@ public class MainActivity extends AppCompatActivity {
         authorTextView = findViewById(R.id.authorTextView);
         fetchQuoteButton = findViewById(R.id.fetchQuoteButton);
         fetchQuoteButton.setOnClickListener(v -> fetchQuote());
+    }
+
+    private boolean isNotifPermissionGranted() {
+        return ActivityCompat.checkSelfPermission(this, NOTIF_PERM) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestNotifPermission() {
+        if (BuildConfig.BUILD_TYPE.equals("release")
+                && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+                && !isNotifPermissionGranted()) {
+            permissionLauncher.launch(NOTIF_PERM);
+        }
+    }
+
+    private void showFeedbackNotification() {
+        if (BuildConfig.BUILD_TYPE.equals("release") && isNotifPermissionGranted()) {
+            distribution.showFeedbackNotification("Please leave your feedback here.", InterruptionLevel.HIGH);
+        }
     }
 
     private void fetchQuote() {
@@ -67,18 +115,18 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     String jsonData = response.body().string();
                     JSONArray jsonArray = new JSONArray(jsonData);
-                    if (jsonArray.length() > 0) {
-                        JSONObject quoteObject = jsonArray.getJSONObject(0);
-                        final String quote = quoteObject.getString("quote");
-                        final String author = quoteObject.getString("author");
-
-                        runOnUiThread(() -> {
-                            quoteTextView.setText(quote);
-                            authorTextView.setText(author);
-                        });
-                    } else {
+                    if (jsonArray.length() == 0) {
                         runOnUiThread(() -> quoteTextView.setText("No quotes found"));
+                        return;
                     }
+                    JSONObject quoteObject = jsonArray.getJSONObject(0);
+                    final String quote = quoteObject.getString("quote");
+                    final String author = quoteObject.getString("author");
+
+                    runOnUiThread(() -> {
+                        quoteTextView.setText(quote);
+                        authorTextView.setText(author);
+                    });
                 } catch (Exception e) {
                     e.printStackTrace();
                     runOnUiThread(() -> quoteTextView.setText("Parse error"));
